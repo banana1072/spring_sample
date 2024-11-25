fbf0dd348548bd655300a2e2592510ed8669595c
[LULL]立花隼人 <h.tachibana@lull-inc.co.jp>
2024-11-26 00:14:04 +0900

    ログイン機能

--- src/main/java/com/spring_sampel/pracitce/controller/IndexController.java
+++ src/main/java/com/spring_sampel/pracitce/controller/IndexController.java
@@ -6,8 +6,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class IndexController {
 
-	@GetMapping("/")
-	public String index() {
-		return "index";
+	@GetMapping("/login")
+	public String showLoginForm() {
+
+		return "login";
 	}
+
 }
